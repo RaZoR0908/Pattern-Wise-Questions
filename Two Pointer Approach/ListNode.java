815c484c7d008a4322041483915b4e7ce5949b98
RaZoR0908 <kadamyash154@gmail.com>
2026-04-18 12:08:05 +0530

    Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.

--- Two Pointer Approach/ListNode.java
+++ Two Pointer Approach/ListNode.java
@@ -1,5 +1,29 @@
 package PatternWiseQue;
-
+/*
+ Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+*/
 public class ListNode {
-
+	public ListNode deleteDuplicates(ListNode head) {
+        if(head==null){
+            return head;
+        }
+        ListNode current =head;
+        while(current !=null && current.next !=null){
+            if(current.val==current.next.val){
+                current.next=current.next.next;
+            }
+            else{
+                current=current.next;
+            }
+        }
+       return head;
+    }
 }
